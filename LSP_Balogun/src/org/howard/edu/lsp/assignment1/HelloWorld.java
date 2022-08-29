5e807ad55007fed2f838d0e7af500c70b2d34d4e
Shady-Rouk <olaoluwa429@gmail.com>
2022-08-28 22:51:30 -0400

    This is my commit for assignment1.

---
+++ LSP_Balogun/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Farouk Balogun
+ * 
+ * @author olaol
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
